9cc3580cccede56ee8971a2e59afa5f845613146
Alamin934 <islamalamin934@gmail.com>
2022-06-01 16:31:12 +0600

    create state and fetch users from nodejs api.display user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
   return (
     <div className="App">
-
+      <h2>Users Founded: {users.length}</h2>
+      <ul>
+        {
+          users.map(user => <li key={user.id}>{user.name} : {user.email}</li>)
+        }
+      </ul>
     </div>
   );
 }
