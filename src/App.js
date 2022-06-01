7c90fda5f7bfa25629e2965f622c52d98b2a2209
Alamin934 <islamalamin934@gmail.com>
2022-06-01 16:55:34 +0600

    create new user form and add create post method for send data to server

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
 
   const [users, setUsers] = useState([]);
+  const nameRef = useRef();
+  const emailRef = useRef();
 
   useEffect(() => {
     fetch('http://localhost:5000/users')
       .then(res => res.json())
       .then(data => setUsers(data))
-  }, [])
+  }, []);
+
+  const handleSubmit = e => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUser = { name: name, email: email };
+    //send data to the server
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then()
+
+    e.preventDefault();
+  }
+
 
   return (
     <div className="App">
       <h2>Users Founded: {users.length}</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Add User" ref={nameRef} />
+        <input type="email" placeholder="Add Email" ref={emailRef} />
+        <input type="submit" />
+      </form>
+
       <ul>
         {
           users.map(user => <li key={user.id}>{user.name} : {user.email}</li>)
